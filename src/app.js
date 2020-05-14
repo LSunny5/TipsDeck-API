6fc4fdf3ec28edfa167cedd0dad85f819ada34e1
Sunny Lee <leesunny507@gmail.com>
2020-05-13 21:00:35 -0400

    edited code with token, logger, and setting up for initial deploy

--- src/app.js
+++ src/app.js
@@ -4,26 +4,20 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 const { NODE_ENV } = require('./config')
+const validateBearerToken = require('./validate-token');
+const errorHandler = require('./error-handler');
 
 const app = express();
 
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', { skip: () => NODE_ENV === 'test' }))
 app.use(helmet());
 app.use(cors());
+app.use(validateBearerToken);
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
+app.get('/api/*', (req, res) => {
+    res.json({ok:true});
 })
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-        response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
+app.use(errorHandler);
 
 module.exports = app;
