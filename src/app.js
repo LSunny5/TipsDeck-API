a833c797bd80df85a317c7d8766ff2df19204959
Sunny Lee <leesunny507@gmail.com>
2020-05-18 17:02:10 -0400

    hooking up to app

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const { NODE_ENV } = require('./config')
 
-//const { CLIENT_ORIGIN } = require('./config') //instead of NODE_ENV
+const { CLIENT_ORIGIN } = require('./config') //instead of NODE_ENV
 const validateBearerToken = require('./validate-token');
 const errorHandler = require('./error-handler');
 
@@ -14,15 +14,12 @@ const tipsRouter = require('../tips/tips-router');
 
 const app = express();
 
-//const morganSetting = process.env.CLENT_ORIGIN === 'production' ? 'tiny' : 'common';
-//app.use(morgan(morganSetting));
-
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', { skip: () => NODE_ENV === 'test' }))
 app.use(helmet());
 app.use(cors());
 app.use(validateBearerToken);
 
-//app.use(cors({origin: CLIENT_ORIGIN}));
+app.use(cors({origin: CLIENT_ORIGIN}));
 
 app.get('/api', (req, res) => {
     res.json({ok:true});
--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'false-api-token',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://admin@localhost/TipsDeck',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL  || 'postgresql://admin@localhost/TipsDeck-test'
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL  || 'postgresql://admin@localhost/TipsDeck-test',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:3000/api"
   }
